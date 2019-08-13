83b6348501734fb60e5aba5f9e608253f83da75a
PetroBoiko <bojko2009@meta.ua>
2019-08-13 06:19:36 +0000

    refresh pages

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-document.querySelector('.show-contacts').addEventListener('click', getHtmlAjax);
-const XHR_STATE_DONE = 4;
-const HTTP_STATUS_CODE = 200;
-function getHtmlAjax() {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XHR_STATE_DONE && xhr.status ===HTTP_STATUS_CODE) {
-        document.querySelector( '.container' ).innerHTML = xhr.responseText;
-        }
-    }
-    xhr.open("GET" , 'my-info.html', true );
-    xhr.send();
+// document.querySelector('.show-contacts').addEventListener('click', getHtmlAjax);
+// const XHR_STATE_DONE = 4;
+// const HTTP_STATUS_CODE = 200;
+// function getHtmlAjax() {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function () {
+//         if (xhr.readyState === XHR_STATE_DONE && xhr.status ===HTTP_STATUS_CODE) {
+//         document.querySelector( '.container' ).innerHTML = xhr.responseText;
+//         }
+//     }
+//     xhr.open("GET" , 'my-info.html', true );
+//     xhr.send();
 }
