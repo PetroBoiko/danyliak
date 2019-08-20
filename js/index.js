ff7db481665eb60584f6d44ca8784d40bcadc3ac
PetroBoiko <bojko2009@meta.ua>
2019-08-20 04:13:16 +0000

    add json

--- js/index.js
+++ js/index.js
@@ -1,15 +1,8 @@
-// document.querySelector('.show-contacts').addEventListener('click', getHtmlAjax);
-// const XHR_STATE_DONE = 4;
-// const HTTP_STATUS_CODE = 200;
-// function getHtmlAjax() {
-//     const xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState === XHR_STATE_DONE && xhr.status ===HTTP_STATUS_CODE) {
-//         document.querySelector( '.container' ).innerHTML = xhr.responseText;
-//         }
-//     }
-//     xhr.open("GET" , 'my-info.html', true );
-//     xhr.send();
-//}
-
 new WOW().init();
+
+const cart = new Cart($('#cartModal'));
+const productList = new ProductList(
+  'products.json',
+  $('.products-container'),
+  cart
+);
